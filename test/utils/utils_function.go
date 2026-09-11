5a5c37703e11babcd4143ff4f8c947f3020730c8
Shubham Minglani <shubham@linux.com>
2026-09-11 13:24:50 +0530

    Simplify dataStorageDirectory mount path from /data/cache to /data

--- test/utils/utils_function.go
+++ test/utils/utils_function.go
@@ -25,7 +25,7 @@ func GetControllerConfigResource() *v1alpha1.ControllerConfig {
 			SecretRef:                   "operator-secret",
 			DoclingServeURL:             "http://docling-serve:5001",
 			DataStorageBucket:           "data-storage-bucket",
-			DataStorageDirectory:        "/data/cache/",
+			DataStorageDirectory:        "/data",
 			MaxConcurrentDoclingTasks:   3,
 			MaxConcurrentLangchainTasks: 3,
 		},
